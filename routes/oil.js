2707afc9cfa1dad05dade7166ace5107d173d4ab
Saimithilesh1 <saimithilesh111@gmail.com>
2022-11-02 20:58:36 -0500

    added files

--- routes/oil.js
+++ routes/oil.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Oil', { title: 'search results for Oil class' });
+  res.render('oil', { title: 'search results for Oil class' });
 
 });
 module.exports = router;
